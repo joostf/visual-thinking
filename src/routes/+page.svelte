f0c4bd89caadd44fa0cc179c80e9601c152ebd0e
iBadr49 <badr.amara@hva.nl>
2024-03-06 10:30:09 +0100

    loading state bug fix v2
    - ik heb de animatie sneller en de scale veranderd zodat ik op de logo komt de staan

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,53 +1,61 @@
 <script>
-    import { onMount, onDestroy } from "svelte";
+  import { onMount, onDestroy } from "svelte";
 
-    import Header from "$lib/molecules/header.svelte";
-    import Footer from "$lib/molecules/footer.svelte";
+  import Header from "$lib/molecules/header.svelte";
 
-    import Homepage from "$lib/organisms/homepage.svelte";
+  import Footer from "$lib/molecules/footer.svelte";
 
-    import LoadingScreen from "$lib/organisms/loadingScreen.svelte";
+  import Homepage from "$lib/organisms/homepage.svelte";
 
-    let loading = true;
+  import LoadingScreen from "$lib/organisms/loadingScreen.svelte";
 
-    function handleKeyDown(event) {
-        if (event.key === "L" || event.key === "l") {
-            loading = true;
-            setTimeout(() => {
-                loading = false;
-            }, 6500);
-        }
+  let loading = true;
+
+  function handleKeyDown(event) {
+    if (event.key === "L" || event.key === "l") {
+      loading = true;
+
+      setTimeout(() => {
+        loading = false;
+      }, 5700);
     }
+  }
 
-    onMount(() => {
-        if (typeof window !== "undefined") {
-            window.addEventListener("keydown", handleKeyDown);
+  onMount(() => {
+    if (typeof window !== "undefined") {
+      window.addEventListener("keydown", handleKeyDown);
 
-            if (sessionStorage.getItem("alreadyLoaded")) {
-                loading = false;
-            } else {
-                sessionStorage.setItem("alreadyLoaded", "true");
-                setTimeout(() => {
-                    loading = false;
-                }, 6500);
-            }
-        }
-    });
+      if (sessionStorage.getItem("alreadyLoaded")) {
+        loading = false;
+      } else {
+        sessionStorage.setItem("alreadyLoaded", "true");
 
-    onDestroy(() => {
-        if (typeof window !== "undefined") {
-            window.removeEventListener("keydown", handleKeyDown);
-        }
-    });
+        setTimeout(() => {
+          loading = false;
+        }, 5700);
+      }
+    }
+  });
+
+  onDestroy(() => {
+    if (typeof window !== "undefined") {
+      window.removeEventListener("keydown", handleKeyDown);
+    }
+  });
 </script>
 
 {#if loading}
-    <LoadingScreen />
-    <Header {loading}/>
-    <Homepage {loading}/>
-    <Footer />
+  <LoadingScreen />
+
+  <Header {loading} />
+
+  <Homepage {loading} />
+
+  <Footer />
 {:else}
-    <Header {loading}/>
-    <Homepage {loading}/>
-    <Footer />
+  <Header {loading} />
+
+  <Homepage {loading} />
+
+  <Footer />
 {/if}
