e9712e396fda805dfb633f8176558407a35b9584
iBadr49 <badr.amara@hva.nl>
2024-03-14 21:54:19 +0100

    Nieuwe component
    - nieuw component die ik gebruik van ga maken op alle pagina's.

---
+++ src/lib/atoms/breadcrumb.svelte
@@ -0,0 +1,48 @@
+<script>
+  export let titel;
+  export let url;
+</script>
+
+{#if url}
+  <a href="{url}">{titel}</a>
+{:else}
+  <span>{titel}</span>
+{/if}
+
+<style>
+  /* MOBILE - STYLING */
+  a,span {
+    font-weight: bold;
+    text-transform: uppercase;
+    /* background-color: var(--vtDarkBlue); */
+    color: var(--vtWhite);
+    font-family: var(--vtPrimaryFont);
+    font-size: 0.9rem;
+    padding-left: 9%;
+    padding-top: 0.2rem;
+    padding-bottom: 0.2rem;
+    margin-top: 0%;
+    width: 100vw;
+    display: flex;
+    align-items: center;
+    text-decoration:none;
+  }
+
+  a:hover {
+    text-decoration: underline;
+  }
+
+  /* tablet */
+  @media (min-width: 31em) and (max-width: 70em) {
+    a, span {
+      padding-left: 20%;
+    }
+  }
+
+  /* mobile */
+  @media (max-width: 31em) {
+    a, span {
+      padding-left: 11%;
+    }
+  }
+</style>
