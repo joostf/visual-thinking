a21240dca9adbb85124d6deccf9f48ac3a72bc62
joostf <j.faber@hva.nl>
2024-03-25 14:41:38 +0100

    :construction: cleaning up

--- src/lib/molecules/header.svelte
+++ src/lib/molecules/header.svelte
@@ -2,7 +2,7 @@
   let active = false;
 </script>
 
-<a href="/" class="logo"><img src="/vtHBO-v1.0-Logo_1.svg"  alt="Visual Thinking" /></a>
+<a href="/" class="logo"><img src="/vtHBO-v1.0-Logo_1.svg"  alt="Visual Thinking Logo" /></a>
 
 <nav>
   <ul>
@@ -21,19 +21,22 @@
     text-decoration: none;
     cursor: pointer;
     font-size: clamp(.8rem, -1.5rem + 8vw, 1rem);
+    padding:.25rem;
   }
 
-  a:hover {
-    text-decoration: underline;
+  a:hover,
+  a:focus-visible {
+    background: var(--vtYellow);
+    outline:none;
   }
 
   a.logo {
     position:absolute;
     top:1rem;
     left:1rem;
-    width:40px;
     z-index:1
   }
+
   a.logo img {
     width:60px
   }
@@ -41,8 +44,8 @@
   nav {
     --horizontal-spacing:1rem;
     padding:1rem 1rem 1rem 3.5rem;
-    margin-left:1rem;
     position: relative;
+    box-shadow: 0 7px 6px -2px var(--vtGrey);
   }
 
   nav::before,
@@ -55,12 +58,12 @@
     height: 100%;
 
     background: rgb(255,255,255);
-    background: linear-gradient(90deg, rgba(255,255,255,1) 0%, rgba(255,255,255,0) 80%);
+    background: linear-gradient(90deg, rgba(255,255,255,1) 40%, rgba(255,255,255,0) 100%);
   }
 
   nav::after {
     left:auto;
-    right:0;
+    right:-1rem;
     background: linear-gradient(90deg, rgba(255,255,255,0) 0%, rgba(255,255,255,1) 50%);
   }
 
@@ -71,25 +74,25 @@
     font-family: var(--vtPrimaryFont);
     font-weight: 700;
     padding:1em;
-    margin: 0;
+    margin: 0 1rem 0 1rem;
     align-items: center;
     gap:4vw;
     overflow-x:auto;
     width:100%;
   }
 
-  
-
-  /* tablet */
   @media (min-width: 72.5em) {
     a.logo {
       position: absolute;
       z-index:1;
-      left:calc(50% - 40px)
+      left:calc(50% - 40px);
+      top:.5rem;
     }
+
     a.logo img {
       width:80px
     }
+
     nav {
       --horizontal-spacing:10vw;
       padding-left:1rem;
@@ -101,14 +104,16 @@
     }
 
     ul {
+      --spacing-logo:6rem;
       justify-content: center;
     }
 
     li:nth-of-type(3) {
-      margin-right: 60px;
+      margin-right: var(--spacing-logo);
     }
+
     li:nth-of-type(4) {
-      margin-left: 60px;
+      margin-left: var(--spacing-logo);
     }
 
     
